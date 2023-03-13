43a711cba4c970c2e51f759c25d027c61b22193b
xmarcysx <marcinchowaniec21@gmail.com>
2023-03-13 18:46:08 +0100

    StatefulWidget and StatelessWidget learned. Also started working on question app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,53 @@
-import 'package:flutter/cupertino.dart';
-
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget {
-  build(BuildContext context) {}
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What is your favourite color?',
+      'What is your favourite animal?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(children: [
+          Text(questions[questionIndex]),
+          ElevatedButton(
+            child: Text('Answer 1'),
+            onPressed: answerQuestion,
+          ),
+          ElevatedButton(
+            child: Text('Answer 2'),
+            onPressed: answerQuestion,
+          ),
+          ElevatedButton(
+            child: Text('Answer 3'),
+            onPressed: answerQuestion,
+          ),
+        ]),
+      ),
+    );
+  }
 }
